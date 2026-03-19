2dc8007b6564dc045baffdeaa9259e0f02456d62
Erlonidas <eng.erlon.almeida@gmail.com>
2026-03-19 18:48:32 -0300

    feat: add MainState with only contracts for rag 2

--- src/rag/schemas/main_state.py
+++ src/rag/schemas/main_state.py
@@ -11,11 +11,19 @@ class Titles(TypedDict):
 class Rag2Report(TypedDict):
     book_id: str
     sub_question: str
-    rag_report: Optional[str]
+    rag_report: str
+
+
+class InputRag(TypedDict):
+    index_name: str
+    query_vector: List[float]
+    query_text: str
+    book_id: str
+    hybrid_text: Optional[str] | None
     
 
 class MainState(MessagesState):
     possibles_pdfs_titles: Optional[List[Titles]]
-    agent_rag_input: Rag2Report
+    retrieval_rag_input: InputRag
     retrieval_reports: Annotated[List[Rag2Report], add]
 # </Considering only RAG step 2>
