ae40a066e0a7d518fb2788a209fd3cdf0bf9deb5
Erlon <erlon.alm@gmail.com>
2026-06-30 15:40:46 -0300

    bugfix: adapt for python version typings

--- src/rag/schemas/decomposer_structured_output.py
+++ src/rag/schemas/decomposer_structured_output.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
-from typing import List, TypedDict, Optional
+# from typing import List, TypedDict, Optional
+from typing import List, Optional
+from typing_extensions import TypedDict 
 
 class SubQueries(TypedDict):
     book_id: str = Field(description="exact book_id that you what make a search")
